cf6f10353822100d65a2dd3e87e561fc40708e58
Mikhail Dronov <mike@dronov.net>
2023-12-05 22:50:29 +0100

    edits in main page

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
-# @app.route('/about/')
-# def about():
-#     return render_template('about.html')
+@app.route('/bios/')
+def bios():
+    return render_template('bios.html')
+
+@app.route('/restream/')
+def restream():
+    return render_template('restream.html')
